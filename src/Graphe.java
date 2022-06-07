3e8cc25ca9dcb98c32347419ff779a36307f4e2a
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2022-06-07 09:02:02 +0200

    graphe

---
+++ src/Graphe.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public interface Graphe {
+
+    /**
+     * Methode qui retourne les noeuds du graphes
+     * @return
+     */
+    public List<String> listeNoeuds();
+
+    /**
+     * Methode qui retourne la liste des arcs
+     * @param n Le noeud selectionne
+     * @return
+     */
+    public List<Arc> suivants(String n);
+
+}
