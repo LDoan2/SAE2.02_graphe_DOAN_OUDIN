81b66c072978e8e57840dd29e79a30ca2a1597ea
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2022-06-13 11:57:17 +0200

    debut bellman

--- src/BellmanFord.java
+++ src/BellmanFord.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class BellmanFord {
+
+    public Valeur resoudre(Graphe g, String depart){
+        Valeur res;
+
+        for (int i = 0; i < g.listeNoeuds().size(); i++){
+            g.listeNoeuds().get(i).get
+        }
+
+        return res;
+    }
 }
