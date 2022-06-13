02b9b22783b14eb3493ef05b64d4bab73eff59e4
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2022-06-13 14:37:03 +0200

    bellman

--- src/BellmanFord.java
+++ src/BellmanFord.java
@@ -5,13 +5,28 @@ public class BellmanFord {
 
 
     public Valeur resoudre(Graphe g, String depart){
-        for (int n = 0; n < g.listeNoeuds().size(); n ++){
+        // On cree une Valeur qui va servir de res
+        Valeur val = new Valeur();
 
-            for (int i = 0; i < g.suivants(g.listeNoeuds().get(n)).size(); i++){
+        for (int i = 0; i < g.listeNoeuds().size(); i++){
+            val.setValeur(g.listeNoeuds().get(i), Double.MAX_VALUE);
 
+            for (int j = 0; j < g.suivants(g.listeNoeuds().get(i)).size() ; j++){
+                val.setParent(g.listeNoeuds().get(i), null);
             }
+        }
 
+        val.setValeur(depart, 0);
+
+        boolean continuer = true;
+
+        while (continuer){
+            continuer = false;
 
         }
+
+        return val;
     }
+
+
 }
