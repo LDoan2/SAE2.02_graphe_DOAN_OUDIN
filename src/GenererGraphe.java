e55c2fe769ecdca0668f3b08c0932f0d0d151d4e
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2022-06-14 11:22:04 +0200

    generer graphe

--- src/GenererGraphe.java
+++ src/GenererGraphe.java
@@ -1,3 +1,23 @@
-public class GenererGraphe {
+import java.util.List;
 
+public class GenererGraphe implements Graphe{
+
+    List<Noeud> noeuds;
+
+
+    public Graphe generer(int taille){
+        // On cree une liste de noeud temp qui va contenir les noeuds generer aleatoirement
+        // List<Noeud> = new List<Noeud>();
+
+    }
+
+    @Override
+    public List<String> listeNoeuds() {
+        return null;
+    }
+
+    @Override
+    public List<Arc> suivants(String n) {
+        return null;
+    }
 }
