a86a35ec758d372d70da23c94ecf89017a3b3943
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2022-06-16 10:49:15 +0200

    rapport graphe

--- src/MainGenGraph.java
+++ src/MainGenGraph.java
@@ -2,10 +2,24 @@ public class MainGenGraph {
 
     public static void main(String[] args) {
 
-        GenererGraphe gen = new GenererGraphe(5);
+        GenererGraphe gen = new GenererGraphe(10);
 
         System.out.println(gen.toString());
 
         System.out.println(gen.digraph());
+
+
+        Dijkstra dijkstra = new Dijkstra();
+        BellmanFord bellmanFord = new BellmanFord();
+        long debut = System.nanoTime();
+        Valeur val = bellmanFord.resoudre(gen, "2");
+        long fin = System.nanoTime();
+        long duree = fin - debut;
+
+
+
+
+        System.out.println(val.toString());
+        System.out.println(duree);
     }
 }
